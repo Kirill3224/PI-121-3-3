d5882b042151687f3716344beb51dc9399468a54
temandro <9128753@stud.kai.edu.ua>
2026-04-30 14:16:04 +0300

    refactor(DAL): update Order and OrderItem configurations based on review

--- PI.DAL/Configurations/OrderConfiguration.cs
+++ PI.DAL/Configurations/OrderConfiguration.cs
@@ -8,7 +8,20 @@ public class OrderConfiguration : IEntityTypeConfiguration<Order>
 {
     public void Configure(EntityTypeBuilder<Order> builder)
     {
+        builder.ToTable("Orders");
+
         builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.CreatedAt).IsRequired();
+        
+        builder.Property(o => o.Status)
+               .IsRequired()
+               .HasConversion<string>()
+               .HasMaxLength(20);
+               
+        builder.Property(o => o.TotalAmount)
+               .IsRequired()
+               .HasColumnType("decimal(18,2)");
 
         builder.HasOne(o => o.User) 
                .WithMany() 
--- PI.DAL/Configurations/OrderItemConfiguration.cs
+++ PI.DAL/Configurations/OrderItemConfiguration.cs
@@ -8,19 +8,23 @@ public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
 {
     public void Configure(EntityTypeBuilder<OrderItem> builder)
     {
+        builder.ToTable("OrderItems");
+
         builder.HasKey(oi => oi.Id);
+
+        builder.Property(oi => oi.CreatedAt).IsRequired();
+
+        builder.Property(oi => oi.UnitPrice)
+               .IsRequired()
+               .HasColumnType("decimal(18,2)");
+
+        builder.Property(oi => oi.Quantity)
+               .IsRequired();
 
         builder.HasOne(oi => oi.Product)
                .WithMany() 
                .HasForeignKey(oi => oi.ProductId)
                .IsRequired()
                .OnDelete(DeleteBehavior.Restrict); 
-
-        builder.Property(oi => oi.UnitPrice)
-               .HasColumnType("decimal(18,2)")
-               .IsRequired();
-
-        builder.Property(oi => oi.Quantity)
-               .IsRequired();
     }
 }
